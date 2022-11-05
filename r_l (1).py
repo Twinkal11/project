8f02efd6391f232b9aa318c364f477c9fa62dce2
Twinkal desai <113102337+Twinkal11@users.noreply.github.com>
2022-11-05 14:54:22 +0530

    Update r_l (1).py

--- r_l (1).py
+++ r_l (1).py
@@ -34,35 +34,45 @@ class raw_ly:
     status = r'\s(\d{3})\s'
     content_size_pattern = r'\s(\d{4,5})\s'
     referer_pattern = r'("https(\S+)")'
+    user_agent = r'(Mozilla|Dal|Goog|troob|bar)\S*\s\((\w+;?\s+\w+)'
    
-    # user_device_pattern = r'(Mozilla|Dal|Goog|troob|bar)\S*\s\((\w+;?\s+\w+)'
-
-
+    
     self.logs_df = df.withColumn("Row_id", monotonically_increasing_id()) \
             .select("Row_id", regexp_extract('value', host, 1).alias('client/ip'),
-                    regexp_extract('value', time_stamp, 1).alias('timestamp'),
+                    regexp_extract('value', time_stamp, 1).alias('Datetime'),
                     regexp_extract('value', method_uri_protocol_pattern, 1).alias('method'),
                     regexp_extract('value', method_uri_protocol_pattern, 2).alias('request'),
                     # regexp_extract('value', method_uri_protocol_pattern, 3).alias('protocol'),
                     regexp_extract('value', status, 1).cast('integer').alias('status'),
                     regexp_extract('value', content_size_pattern, 1).cast('integer').alias('content_size'),
-                    regexp_extract('value', referer_pattern, 1).alias('referer')
-                    )
-    self.logs_df.show()
+                    regexp_extract('value', referer_pattern, 1).alias('referer'),
+                    regexp_extract('value', user_agent, 2).alias('user_agent')
+               )
+    # self.logs_df.show(truncate=False)
+  
+  def remove_spec(self):
 
+    spe_char = r'[%,|"-&.=?-]'
+    self.logs_df=self.logs_df.withColumn("request", regexp_replace("request", spe_char, ""))\
+    .withColumn("referer",regexp_replace("referer", spe_char, ""))
+    
+    
+                 
+    self.logs_df.show(100)
   def save(self):
 
     self.logs_df.write.mode("overwrite").format("csv").save("/content/drive/MyDrive/raw_layer/raw_l.csv",header="True",mode='overwrite')
 
   def show_On_Hive(self):
     pass
-    self.logs_df.write.option("mode","overwrite").saveAsTable('Raw_D')
-    self.spark.sql("select count(*) from Raw_D").show()
+    self.logs_df.write.option("mode","overwrite").saveAsTable('Raw_lrr')
+    self.spark.sql("select count(*) from Raw_lrr").show()
 
 
 if __name__ == '__main__':
   raw = raw_ly()
   raw.read_csv_file()
+  raw.remove_spec()
   raw.save()
   raw.show_On_Hive()
 
